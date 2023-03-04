a40686879e83c86bf6c8f4eba739abcb488bfac7
matt on M1-MBP <matt.gibson.dev@gmail.com>
2023-03-04 15:30:04 -0800

    Make ConvoStore public

--- Sources/ChatGPTSwift/ConvoStore.swift
+++ Sources/ChatGPTSwift/ConvoStore.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-enum ChatStorageError: Error {
+public enum ChatStorageError: Error {
     case noConvoWithProvidedID
     case couldNotSave
 }
 
 
 // Singleton Access to userdefaults
-actor ConvoStore: ChatStorage {
+public actor ConvoStore: ChatStorage {
     
     //MARK: - ChatStorage
-    func openConversationSnapshot(conversationID: UUID) throws -> Conversation {
+    public func openConversationSnapshot(conversationID: UUID) throws -> Conversation {
         guard let convo = getConversationFor(id: conversationID) else {
             throw ChatStorageError.noConvoWithProvidedID
         }
         return convo
     }
     
-    func saveConversationSnapshot(conversation: Conversation) {
+    public func saveConversationSnapshot(conversation: Conversation) {
         try? save(conversation: conversation)
     }
     //MARK: End ChatStorage -
@@ -34,7 +34,7 @@ actor ConvoStore: ChatStorage {
     private var defaults = UserDefaults.standard
     
     //TODO: use ~better~ datastore
-    var convos: [Conversation] {
+    public var convos: [Conversation] {
         do {
             if let data = defaults.data(forKey: Self.key){
                 let convos = try JSONDecoder().decode(Array<Conversation>.self, from: data)
@@ -46,35 +46,35 @@ actor ConvoStore: ChatStorage {
         return []
     }
     
-    var convoCount: Int {
+    public var convoCount: Int {
         convos.count
     }
     
-    var mostRecentConvo: Conversation? {
+    public var mostRecentConvo: Conversation? {
         return convos.last
     }
     
     /// ConvoStore shared singleton instance
-    static let shared = ConvoStore()
+    public static let shared = ConvoStore()
 
     // Private Initialization for singleton
     private init() {
     }
     
-    func getConvosFor(identifiers: [UUID]) -> [Conversation] {
+    public func getConvosFor(identifiers: [UUID]) -> [Conversation] {
         return convos.filter({identifiers.contains($0.id)})
     }
     
-    func getConversationFor(id: UUID) -> Conversation? {
+    public func getConversationFor(id: UUID) -> Conversation? {
         return convos.first(where: {$0.id == id})
     }
     
-    func getRecentConvos(_ n: Int = 5) -> [Conversation]{
+    public func getRecentConvos(_ n: Int = 5) -> [Conversation]{
         return convos.sorted(by: {$0.lastInteraction < $1.lastInteraction}).suffix(n)
     }
     
     /// Save a new conversation or update an existing one
-    func save(conversation: Conversation) throws {
+    public func save(conversation: Conversation) throws {
             // if this is an existing conversation
         if let existingConvoIndex = convos.firstIndex(where: {$0.id == conversation.id}) {
             var updatedConvos = convos
@@ -90,7 +90,7 @@ actor ConvoStore: ChatStorage {
     }
     
 
-    func removeAllConvos(){
+    public func removeAllConvos(){
         print("Deleting all convos...")
         let updatedConvos: [Conversation] = []
         defaults.set(try! JSONEncoder().encode(updatedConvos), forKey: Self.key)
